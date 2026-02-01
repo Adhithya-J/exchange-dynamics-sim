abe3716ea94b95c9948e7c3c0000c11c93ffb287
Adhithya-J <137282088+Adhithya-J@users.noreply.github.com>
2026-02-01 23:55:09 +0530

    modified resource allocation from sequential to simultaneous in each iteration

--- main.py
+++ main.py
@@ -4,63 +4,75 @@ import numpy as np
 import random
 import shortuuid
 
+GIVING_FLOOR = 1
+RECEIVING_CEIL = 1e4
 
+random.seed(42)
+np.random.seed(42)
 
 # initialization
 def initialize_agents(n=100) -> pd.DataFrame:
     df = pd.DataFrame(columns=["id","generosity_score","acceptance_score","resources"])
     
     generosity_vals = np.linspace(0,1,11) #range(11)
-    acceptance_vals = [1,]
+    acceptance_vals = np.linspace(0,1,11) #[1,]
     max_resources = 1_000
 
-    
+    agents = []
     for i in range(n):
         agent = {}
         agent["id"] = shortuuid.uuid()
         agent["generosity_score"] = random.choice(generosity_vals)
-        agent["acceptance_score"] = acceptance_vals[0]
+        agent["acceptance_score"] = random.choice(acceptance_vals)
         agent["resources"] = max_resources
-        df = pd.concat([df, pd.DataFrame(agent,index=[i])],ignore_index=False)
+        agents.append(agent)
+
+    df = pd.DataFrame(agents)
+    df = df.set_index("id")
     return df
 
 
 def single_iteration(df) -> pd.DataFrame:
     df = df.copy()
-    receivers_list = list(df["id"])
+    agent_ids = list(df.index)
 
-    def find_random_receiver_id(sender_id, receivers):
-        receivers = receivers.copy()
-        idx = receivers.index(sender_id)
-        receivers.pop(idx)
-        return random.choice(receivers)
+    def find_random_receiver_id(sender_id, agent_ids):
+        return random.choice(df.index[df.index != sender_id])
+    
+    
+    def _is_capable_of_giving(row) -> bool:
+        return (row["resources"] > GIVING_FLOOR and random.uniform(0,1) < row["generosity_score"])
 
-    def give(df,giver_id) -> pd.DataFrame:
-        new_df = df.copy()
-        receiver_id = find_random_receiver_id(giver_id, receivers_list)
-        receiver_index = df[df["id"]==receiver_id].index
-        giver_index = df[df["id"]==giver_id].index
-        new_df.loc[receiver_index,"resources"] +=1
-        new_df.loc[giver_index,"resources"] -=1
+    def _is_capable_of_receiving(row) -> bool:
+        return (row["resources"] < RECEIVING_CEIL and random.uniform(0,1) < row["acceptance_score"])
 
-        return new_df
-
-    for index, row in df.iterrows():
+    def generate_transfer_actions(df):
+        actions = []
+        for id in df.index:        
+            if _is_capable_of_giving(df.loc[id]): 
+                receiver = find_random_receiver_id(id, agent_ids)
+                if _is_capable_of_receiving(df.loc[receiver,:]):
+                    actions.append((id, receiver))
+        return actions
         
-        if row["resources"] <= 0: # handles dead agents
-            continue
-
-        tendency = random.uniform(0,1)
+    def perform_transfers(df, actions) -> pd.DataFrame:
         
-        if tendency <= row["generosity_score"]:
-            df = give(df,giver_id=row["id"])
+        resources = df["resources"].copy()
+        
+        for giver, receiver in actions:
+            resources[giver] -= 1
+            resources[receiver] += 1
 
+        df["resources"] = resources        
+        return df
+
+    transfer_actions = generate_transfer_actions(df)
+    df = perform_transfers(df, transfer_actions)
 
     return df
 
 def multiple_iterations(df, n=100):
-    df = df.copy()
-
+    
     for i in range(n):
         df = single_iteration(df)
 
@@ -71,7 +83,7 @@ def main():
     df = initialize_agents(n = 10)
     print(df.head(10))
     print("--"*10)
-    df_1 = multiple_iterations(df, n=2_000)
+    df_1 = multiple_iterations(df, n=10_000)
     print(df_1.head(10))
 
 
